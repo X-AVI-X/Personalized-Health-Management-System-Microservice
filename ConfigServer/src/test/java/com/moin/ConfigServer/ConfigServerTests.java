3620fca4f47c1c8ea9f58a18671befb0eaf3969e
Avijit Paul <er.avijit18@gmail.com>
2024-10-06 06:39:25 +0600

    Add files via upload

---
+++ ConfigServer/src/test/java/com/moin/ConfigServer/ConfigServerTests.java
@@ -0,0 +1,13 @@
+package com.moin.ConfigServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
